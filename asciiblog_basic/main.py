b5d3ad9cc03ddb186ef0c606411460a175b1bf87
Adam <adamh93@verizon.net>
2016-07-05 23:15:35 -0400

    Basic ASCII Blog form that replies if you provide the correct information with thanks otherwise it will redirect user to re-input the correct info.

---
+++ asciiblog_basic/main.py
@@ -0,0 +1,39 @@
+import os
+import jinja2
+import webapp2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+#Jinja Template Handler
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a,**kw)
+        
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+    def render_front(self, title="", art="", error=""):
+        self.render("front.html", title=title, art=art, error=error)
+    def get(self):
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        art = self.request.get("art")
+
+        if title and art:
+            self.write("Thanks")
+        else:
+            error = "we need both a title and some artwork!"
+            self.render_front(error = error)
+
+        
+app = webapp2.WSGIApplication([('/', MainPage)
+                               ], debug=True)
